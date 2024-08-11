7e67e75d0e050256d5062ee427a0360055ecbceb
Kinomagnum <kinomagnum@gmail.com>
2024-08-11 20:18:10 +0300

    initialCommit

---
+++ src/test/java/ru/netology/BonusServiceTest.java
@@ -0,0 +1,41 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BonusServiceTest {
+
+    @Test
+    void calculateBonus() {
+        BonusService service = new BonusService();
+        int amount = 2000;
+
+        int actual =service.calculateBonus(amount);
+        int expected = 10;
+
+        assertEquals(expected, actual);
+
+        //assertEquals(10, actual);
+    }
+    @Test
+    void calculateBonus1() {
+        BonusService service = new BonusService();
+        int amount = 100000;
+
+        int actual =service.calculateBonus(amount);
+        int expected = 990;
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void calculateBonus2() {
+        BonusService service = new BonusService();
+        int amount = 102000;
+
+        int actual =service.calculateBonus(amount);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
+}
